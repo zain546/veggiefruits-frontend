9172a1824d3246519e1ce332df73b973ad7e0d83
Zain Ali <zainali187483@gmail.com>
2025-05-21 13:51:02 +0500

    ✨  Add folder structure and assets

---
+++ public/assets/index.ts
@@ -0,0 +1,59 @@
+import bakery_img from './PNGs/bakery_img.png';
+import bottles_img from './PNGs/bottles_img.png';
+import bottom_banner_img_sm from './PNGs/bottom_banner_img_sm.png';
+import bottom_banner from './PNGs/bottom_banner.png';
+import carrots from './PNGs/carrots.png';
+import dairy_product_img from './PNGs/dairy_product_img.png';
+import fresh_fruits_img from './PNGs/fresh_fruits_img.png';
+import grain_img from './PNGs/grain_img.png';
+import green_leafs from './PNGs/green_leafs.png';
+import maggi_img from './PNGs/maggi_img.png';
+import main_banner_bg from './PNGs/main_banner_bg.png';
+import onions from './PNGs/onions.png';
+import organic_vegitables_img from './PNGs/organic_vegitables_img.png';
+import potatos from './PNGs/potatos.png';
+import profile_icon from './PNGs/profile_icon.png';
+import tomatos from './PNGs/tomatos.png';
+import cart_icon from './SVGs/cart_icon.svg';
+import cart from './SVGs/cart.svg';
+import coin_icon_D from './SVGs/coin_icon_D.svg';
+import delivery_truck_icon from './SVGs/delivery_truck_icon.svg';
+import green_start_icon from './SVGs/green_start_icon.svg';
+import hamburgur from './SVGs/hamburgur.svg';
+import leaf_icon from './SVGs/leaf_icon.svg';
+import right_arrow_icon from './SVGs/right_arrow_icon.svg';
+import right_arror_white from './SVGs/right_arror_white.svg';
+import search_icon from './SVGs/search_icon.svg';
+import star_icon from './SVGs/star_icon.svg';
+import trust_icon from './SVGs/trust_icon.svg';
+
+export {
+  bakery_img,
+  bottles_img,
+  bottom_banner_img_sm,
+  bottom_banner,
+  carrots,
+  dairy_product_img,
+  fresh_fruits_img,
+  grain_img,
+  green_leafs,
+  maggi_img,
+  main_banner_bg,
+  onions,
+  organic_vegitables_img,
+  potatos,
+  profile_icon,
+  tomatos,
+  cart_icon,
+  cart,
+  coin_icon_D,
+  delivery_truck_icon,
+  green_start_icon,
+  hamburgur,
+  leaf_icon,
+  right_arrow_icon,
+  right_arror_white,
+  search_icon,
+  star_icon,
+  trust_icon,
+};
